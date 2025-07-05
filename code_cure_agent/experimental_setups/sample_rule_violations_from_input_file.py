141511c52760db18ff239804d9f9326cd460d8ee
Pascal-Joos <pascal.joos12@outlook.com>
2025-07-05 12:32:44 +0000

    Implement filling up the dataset to x samples randomly and use it to create the input file filled up to 1000 samples

--- code_cure_agent/experimental_setups/sample_rule_violations_from_input_file.py
+++ code_cure_agent/experimental_setups/sample_rule_violations_from_input_file.py
@@ -7,6 +7,8 @@ import csv
 # Samples 'samples_per_rule' many samples from each of the rules that occur in the input file (at most max_number_of_considered_rules rules).
 # Renumbers the sampled instances
 
+# If --fill-up-to is set to a number, then the csv file is filled up with further samples, that are sampled completely random (not uniform by rule).
+
 
 @click.command()
 @click.argument(
@@ -31,7 +33,13 @@ import csv
     default=-1,
     help="Number of rules that are at most considered. If not specified, then all rules with at least samples_per_rule samples are considered."
 )
-def sample_rule_violations_from_input_file(csv_input_file: click.File, target_csv_file_path: str, samples_per_rule: int, max_number_of_considered_rules: int):
+@click.option(
+    "--fill-up-to",
+    type=int,
+    default=-1,
+    help="Number of samples to fill the csv up to"
+)
+def sample_rule_violations_from_input_file(csv_input_file: click.File, target_csv_file_path: str, samples_per_rule: int, max_number_of_considered_rules: int, fill_up_to: int):
 
     number_generator = np.random.Generator(np.random.PCG64(seed=42))
 
@@ -65,18 +73,26 @@ def sample_rule_violations_from_input_file(csv_input_file: click.File, target_cs
     df_sampled_rule_instances = (
         df_only_rules_with_sufficient_instances.groupby("ruleKey")
         .sample(n=samples_per_rule, random_state=number_generator)
-        .reset_index(drop=True)
     )
 
     df_sampled_rule_instances_shuffled = df_sampled_rule_instances.sample(
         frac=1, random_state=number_generator)
 
+    if fill_up_to != -1 and fill_up_to > len(df_sampled_rule_instances_shuffled):
+        # Fill up the samples with comletely random samples, that haven't occured before
+        df_unsampled_instances = input_file_df.drop(
+            df_sampled_rule_instances_shuffled.index)
+        df_filled_up = pd.concat([df_sampled_rule_instances_shuffled, df_unsampled_instances.sample(
+            n=fill_up_to - len(df_sampled_rule_instances_shuffled), random_state=number_generator)]).reset_index(drop=True)
+    else:
+        df_filled_up = df_sampled_rule_instances_shuffled
+
     # Change the istanceID to consecutive numbers
-    df_sampled_rule_instances_shuffled['instanceID'] = range(
-        1, len(df_sampled_rule_instances_shuffled) + 1)
+    df_filled_up['instanceID'] = range(
+        1, len(df_filled_up) + 1)
 
     # output to csv
-    df_sampled_rule_instances_shuffled.to_csv(
+    df_filled_up.to_csv(
         target_csv_file_path, encoding="utf-8", index=False, header=True, quoting=csv.QUOTE_ALL, quotechar='"',
         doublequote=True)
     print(f"Sampled data saved to {target_csv_file_path}")

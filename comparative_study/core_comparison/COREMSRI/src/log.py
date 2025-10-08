b0390803ded140c7dd63fa831d758d2a1789c8b3
Pascal-Joos <pascal.joos12@outlook.com>
2025-10-08 12:12:22 +0000

    Remove logging of API key for CORE

--- comparative_study/core_comparison/COREMSRI/src/log.py
+++ comparative_study/core_comparison/COREMSRI/src/log.py
@@ -11,18 +11,24 @@ class Log:
         self.fname = fname
 
     def create_json(self, params, record):
-        logging.info(f"Logging results to {self.exp_log_lib / f'{self.fname}.json'}")
+        logging.info(
+            f"Logging results to {self.exp_log_lib / f'{self.fname}.json'}")
         exp_results = {
             "params": params,
             "record": record,
         }
+        # Remove the api_key from params before saving
+        if "api_key" in exp_results["params"]:
+            exp_results["params"].pop("api_key")
+
         with open(self.exp_log_lib / f"{self.fname}.json", "w") as f:
             json.dump(exp_results, f)
 
         return exp_results
 
     def create_log_pyFile(self, exp_results):
-        logging.info(f"Logging results to {self.exp_log_lib / f'{self.fname}.log'}")
+        logging.info(
+            f"Logging results to {self.exp_log_lib / f'{self.fname}.log'}")
         with open(self.exp_log_lib / f"{self.fname}.log", "w", encoding="utf-8") as f:
             f.write(
                 f"Results for experiment with parameters -\n{exp_results['params']}\n"
@@ -34,9 +40,11 @@ class Log:
                     f"\n\n\n======================== Prompt {i} ========================\n"
                 )
                 f.write(prompt)
-                f.write(f"\n------------ Result for Prompt {i} ------------\n\n")
+                f.write(
+                    f"\n------------ Result for Prompt {i} ------------\n\n")
                 f.write(str(result.text))
-                f.write(f"\n\n------------ End Result for Prompt {i} ------------\n\n")
+                f.write(
+                    f"\n\n------------ End Result for Prompt {i} ------------\n\n")
                 f.write(f"Finish Reason: {result.finish_reason}\n\n")
                 f.write(
                     f"======================== End Prompt {i} ========================\n"

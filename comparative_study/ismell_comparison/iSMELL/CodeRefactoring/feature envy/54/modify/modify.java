4b903858458fa05b6ca17f114255503a0f73d535
Pascal-Joos <pascal.joos12@outlook.com>
2026-02-11 15:36:22 +0000

    remove the original iSMELL

--- comparative_study/ismell_comparison/iSMELL/CodeRefactoring/feature envy/54/modify/modify.java
+++
@@ -1,4 +0,0 @@
-public synchronized void renderScene(Scene theScene, Camera camera, RenderListener rl, SceneCamera sceneCamera) {
-        RenderManager renderManager = new RenderManager(new CameraService(), new RenderSetupService());
-        renderManager.renderScene(theScene, camera, rl, sceneCamera);
-        }
